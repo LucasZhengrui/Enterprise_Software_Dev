4c2e5acb8b98f95cf0dcaccacba159ae757c6a06
wangleiz166 <wangleiz166@126.com>
2023-03-17 02:46:17 +0000

    add

--- disaster_list/models.py
+++ disaster_list/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class DisasterList(models.Model):
+    dis_id = models.IntegerField(primary_key=True)
+    year = models.IntegerField()
+    disaster_group = models.CharField(max_length=50)
+    disaster_type = models.CharField(max_length=50)
+    country = models.CharField(max_length=100)
+    iso = models.CharField(max_length=3)
+    total_affected = models.IntegerField()
+    total_damages_usd = models.FloatField()
+
+    class Meta:
+        db_table = 'disaster_list'
+
+    def __str__(self):
+        return f"{self.disaster_type} - {self.country} - {self.year}"
--- disaster_list/views.py
+++ disaster_list/views.py
@@ -1,12 +1,43 @@
 from django.shortcuts import render
+from .models import DisasterList
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# Create your views here.
-# use f-strings for easy string formatting https://realpython.com/python-f-strings/ 
+def get_disasters():
+    disasters = DisasterList.objects.order_by('dis_id')
+    return disasters
 
-def list():
-    list = []
-    return list
+from django.shortcuts import render
+from .models import DisasterList
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+def get_disasters():
+    disasters = DisasterList.objects.order_by('dis_id')
+    return disasters
 
 def index(request):
-    myList = list()
-    return render(request, 'disaster_list/index.html', {'list': myList})
+    disasters_list = get_disasters()
+
+    # Pagination
+    paginator = Paginator(disasters_list, 10)  # Show 10 items per page
+    page = request.GET.get('page', 1)
+
+    try:
+        # Convert page to an integer
+        page = int(page)
+        # Validate that the page is greater than or equal to 1
+        if page <= 0:
+            page = 1
+        # Get the page from the paginator
+        disasters = paginator.page(page)
+    except (PageNotAnInteger, EmptyPage):
+        # If page is not an integer or out of range, deliver first page.
+        disasters = paginator.page(1)
+
+    return render(request, 'disaster_list/index.html', {'disasters': disasters})
+
+
+
+
+
+
+
