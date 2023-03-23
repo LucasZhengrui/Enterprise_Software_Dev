43b9483e04aaeb88967e7b75ec37bcfe43368489
wangleiz166 <wangleiz166@126.com>
2023-03-23 02:22:49 +0000

    add csv function

--- disaster_list/urls.py
+++ disaster_list/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.index, name='search'),
-    path('delete/<int:dis_id>/', views.delete, name='delete'),  
+    path('delete/<int:dis_id>/', views.delete, name='delete'),
+    path('download/<int:page_id>/<str:search_field>/<str:search_query>/', views.download, name='download'),   
 ]
--- disaster_list/views.py
+++ disaster_list/views.py
@@ -1,3 +1,5 @@
+import csv
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from .models import summary as DisasterList
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -43,6 +45,44 @@ def delete(request, dis_id):
     disaster.save()
     return index(request)
 
+def download(request,  page_id, search_field, search_query):
+    # Query the data of the form to be downloaded
+    disasters_list = get_disasters()
+
+    # Search
+    if search_query and search_field != 'all':
+        disasters_list = disasters_list.filter(**{search_field: search_query})
+
+    # Pagination
+    paginator = Paginator(disasters_list, 10)  # Show 10 items per page
+    page = page_id
+    
+    try:
+        # Convert page to an integer
+        page = int(page)
+        # Validate that the page is greater than or equal to 1
+        if page <= 0:
+            page = 1
+        # Get the page from the paginator
+        disasters = paginator.page(page)
+    except (PageNotAnInteger, EmptyPage):
+        # If page is not an integer or out of range, deliver first page.
+        disasters = paginator.page(1)
+
+    # Create the HttpResponse object
+    response = HttpResponse(content_type='text/csv')
+    table_name = "Alldata_table_"+str(page_id)+".csv"
+    response['Content-Disposition'] = 'attachment; filename='+table_name
+
+    # Write to table data
+    writer = csv.writer(response)
+    for row in disasters:
+        writer.writerow([
+            row.Dis_ID, row.Year, row.Disaster_Group, row.Disaster_Type,
+            row.Country, row.ISO, row.Total_Affected, row.Total_Damages
+        ])
+
+    return response
 
 
 
