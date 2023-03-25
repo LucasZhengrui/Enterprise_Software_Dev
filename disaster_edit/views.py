f4a5f05abf81663259746c64fceb842608540142
wangleiz166 <wangleiz166@126.com>
2023-03-25 01:03:35 +0000

    fix

--- disaster_edit/views.py
+++ disaster_edit/views.py
@@ -8,9 +8,9 @@ from django.db import connection
 def index(request, show_id):
     connection = sqlite3.connect('db.sqlite3')
     cursor = connection.cursor()
-    cursor.execute('SELECT * FROM disaster_table_summary AS a LEFT JOIN disaster_table_details AS b ON a.id = b.Dis_ID_id WHERE a.Dis_ID = ?', (show_id,))
+    cursor.execute('SELECT * FROM disaster_table_summary AS a LEFT JOIN disaster_table_details AS b ON a.id = b.Dis_ID_id WHERE a.id = ?', (show_id,))
     myList = cursor.fetchall()
-    # print(myList) # Check how the data show
+    print(myList) # Check how the data show
     connection.close()
     return render(request, 'disaster_edit/index.html', {'list': myList})
 
